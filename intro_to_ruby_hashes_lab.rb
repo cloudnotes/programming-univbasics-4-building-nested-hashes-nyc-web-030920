4337168f79f9d64bd98f55deb3098ef7f1301fb5
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 18:04:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,4 +34,5 @@ end
 def monopoly_with_fourth_tier
   monopoly_with_third_tier
   [:railroads][:names][:reading_railroad]["mortgage_value"] => "$100"
+  [:railroads][:names][:pennsylvania_railroad]["mortgage_value"] => "$200"
 end
