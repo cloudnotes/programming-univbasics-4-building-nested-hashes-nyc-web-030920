0d536870db838f33f32b8e495da5a030d3b2a3a2
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-03-04 17:41:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,7 @@ def monopoly_with_third_tier
     :three_pieces_owed => 100,
     :four_pieces_owed => 200
   }
+  base_hash[:railroads][:names] 
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
